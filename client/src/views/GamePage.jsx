e488645492504796cc8e93782992998e29ccae94
SaintMichael21 <saaintmichael@gmail.com>
2024-04-23 14:36:14 +0700

    Client Setup

---
+++ client/src/views/GamePage.jsx
@@ -0,0 +1,11 @@
+const GamePage = () => {
+  return (
+    <>
+      <div>
+        <h1>Ini GamePage</h1>
+      </div>
+    </>
+  );
+};
+
+export default GamePage;
---
+++ client/src/views/LeaderBoard.jsx
@@ -0,0 +1,11 @@
+const LeaderBoard = () => {
+  return (
+    <>
+      <div>
+        <h1>Ini LeaderBoard Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default LeaderBoard;
